2b0aab5576b31b252a2200ed4556e51590e213e2
MechaCoder <yassine.amzil15@gmail.com>
2024-11-16 13:09:03 +0100

    Project Initialization:
    - Initial commit for the News Publisher System Spring Boot application. This setup includes basic project structure, dependencies, and configurations to support future development of core features

---
+++ backend-java/NewsPublishSystem/src/main/java/be/pxl/services/NewsPublishSystemApplication.java
@@ -0,0 +1,13 @@
+package be.pxl.services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsPublishSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsPublishSystemApplication.class, args);
+	}
+
+}
