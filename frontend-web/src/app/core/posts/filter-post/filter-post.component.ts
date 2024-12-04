a5535a436aed0e542819d4ba4c0189c8615c9325
MechaCoder <yassine.amzil15@gmail.com>
2024-12-04 20:53:53 +0100

    Feature: Implement filter functionality in FilterPost component with form inputs and event emission

--- frontend-web/src/app/core/posts/filter-post/filter-post.component.ts
+++ frontend-web/src/app/core/posts/filter-post/filter-post.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Filter } from '../../../shared/models/filter.model';
+import { Post } from '../../../shared/models/post.model';
+import { PostService } from '../../../shared/services/post.service';
 
 @Component({
   selector: 'app-filter-post',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './filter-post.component.html',
-  styleUrl: './filter-post.component.css'
+  styleUrl: './filter-post.component.css',
 })
 export class FilterPostComponent {
+  filter: Filter = { content: '', category: '', author: '' };
+  posts: Post[] = [];
 
+  @Output() filterChanged = new EventEmitter<Filter>();
+
+  constructor(private postService: PostService) {}
+
+  onSubmit(form: any) {
+    if (form.valid) {
+      this.filter.content = this.filter.content.toLowerCase();
+      this.filter.category = this.filter.category.toLowerCase();
+      this.filter.author = this.filter.author.toLowerCase();
+      this.filterChanged.emit(this.filter);
+      this.postService.getRelevantPosts(this.filter).subscribe((posts) => {
+        this.posts = posts;
+      });
+    }
+  }
+
+  // onSubmit(form: any) {
+  //   if (form.valid) {
+  //     this.filter.content = this.filter.content.toLowerCase();
+  //     this.filter.category = this.filter.category.toLowerCase();
+  //     this.filter.author = this.filter.author.toLowerCase();
+  //     this.filterChanged.emit(this.filter);
+  //   }
+  // }
 }
