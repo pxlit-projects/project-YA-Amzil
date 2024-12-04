5cb4f1b6985255f0b1add9b385773f89f0e34425
MechaCoder <yassine.amzil15@gmail.com>
2024-12-04 20:51:45 +0100

    Feature: Add Navbar component with routing for Posts and Create Post

---
+++ frontend-web/src/app/core/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+import { RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css',
+})
+export class NavbarComponent {}
