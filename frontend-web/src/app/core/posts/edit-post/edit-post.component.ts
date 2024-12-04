7a291c5415e4b4f1eb5ec58bd73e8aeb2e16cd0e
MechaCoder <yassine.amzil15@gmail.com>
2024-12-04 20:54:04 +0100

    Feature: Refactor Edit Post component to use Reactive Forms with validation and improve UI

--- frontend-web/src/app/core/posts/edit-post/edit-post.component.ts
+++ frontend-web/src/app/core/posts/edit-post/edit-post.component.ts
@@ -1,33 +1,60 @@
-import { Component, NgModule, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { PostService } from '../../../shared/services/post.service';
 import { Post } from '../../../shared/models/post.model';
-import { FormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-edit-post',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './edit-post.component.html',
   styleUrl: './edit-post.component.css',
 })
 export class EditPostComponent implements OnInit {
+  postService: PostService = inject(PostService);
   post!: Post;
-
-  constructor(
-    private route: ActivatedRoute,
-    private postService: PostService
-  ) {}
+  // router: Router = inject(Router);
+  fb: FormBuilder = inject(FormBuilder);
+  postForm: FormGroup = this.fb.group({
+    // id: 0,
+    title: ['', [Validators.required]],
+    content: ['', [Validators.required]],
+    category: ['', [Validators.required]],
+    author: ['', [Validators.required]],
+    // updateAt: new Date(),
+  });
 
   ngOnInit(): void {
-    const id = +this.route.snapshot.paramMap.get('id')!;
-    this.postService.getPost(id).subscribe((data: Post) => {
-      this.post = data;
+    this.post = history.state['post'];
+    this.postForm.patchValue({
+      title: this.post.title,
+      content: this.post.content,
+      category: this.post.category,
+      author: this.post.author,
     });
   }
 
-  updatePost(): void {
-    this.postService.updatePost(this.post).subscribe();
+  onSubmit(): void {
+    if (this.postForm.valid) {
+      const post: Post = this.postForm.value as Post;
+      this.editPost(post);
+    }
   }
+
+  editPost(post: Post) {
+    this.postService.updatePost(post).subscribe(() => {
+      // this.router.navigate(['/posts']);
+    });
+  }
+
+  // cancel(): void {
+  //   this.router.navigate(['/posts']);
+  // }
 }
