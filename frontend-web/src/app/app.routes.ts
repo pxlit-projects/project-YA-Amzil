a7324e6ecbeaca8accd4e7a2a8db93359f7a8939
MechaCoder <yassine.amzil15@gmail.com>
2024-12-04 20:51:21 +0100

    Feature: Add routing for create post and integrate Navbar component

--- frontend-web/src/app/app.routes.ts
+++ frontend-web/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CreatePostComponent } from './core/posts/create-post/create-post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'create',component: CreatePostComponent},
+];
