0febf1949095ccb9884a9ec804b08c5f683e7cde
MechaCoder <yassine.amzil15@gmail.com>
2024-12-05 16:07:02 +0100

    Feature: Add Home component and configure routing for home page

--- frontend-web/src/app/app.component.ts
+++ frontend-web/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./core/home/home.component";
 import { NavbarComponent } from "./core/navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, HomeComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- frontend-web/src/app/app.routes.ts
+++ frontend-web/src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { CreatePostComponent } from './core/posts/create-post/create-post.component';
+import { HomeComponent } from './core/home/home.component';
 
 export const routes: Routes = [
-  { path: 'create',component: CreatePostComponent},
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'create', component: CreatePostComponent },
 ];
