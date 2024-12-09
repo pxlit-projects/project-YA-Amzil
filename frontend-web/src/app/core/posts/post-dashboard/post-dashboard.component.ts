982000b088faf4b65ea3ebe751f9070e48e49c39
MechaCoder <yassine.amzil15@gmail.com>
2024-12-09 18:24:44 +0100

    Feature: Implement post dashboard with filtering and display of draft and pending posts

---
+++ frontend-web/src/app/core/posts/post-dashboard/post-dashboard.component.ts
@@ -0,0 +1,42 @@
+import { Component, inject } from '@angular/core';
+import { PostItemComponent } from "../post-item/post-item.component";
+import { Post } from '../../../shared/models/post.model';
+import { PostService } from '../../../shared/services/post.service';
+import { Filter } from '../../../shared/models/filter.model';
+import { FilterPostComponent } from "../filter-post/filter-post.component";
+
+@Component({
+  selector: 'app-post-dashboard',
+  standalone: true,
+  imports: [PostItemComponent, FilterPostComponent],
+  templateUrl: './post-dashboard.component.html',
+  styleUrl: './post-dashboard.component.css',
+})
+export class PostDashboardComponent {
+  posts!: Post[];
+  filteredData!: Post[];
+  postService: PostService = inject(PostService);
+
+  ngOnInit(): void {
+    this.getPosts();
+  }
+
+  handleFilter(filter: Filter) {
+    this.postService.filterPublishedPosts(filter).subscribe({
+      next: (posts) => {
+        this.filteredData = posts;
+        posts;
+        console.log('Filtered Posts:', this.filteredData);
+      },
+    });
+  }
+
+  getPosts() {
+    this.postService.getAllDraftAndPendingPosts().subscribe({
+      next: (posts) => {
+        // this.posts = posts; // --> Hier krijg mij lijst van posts te zien
+        this.filteredData = posts; // --> Hier krijg ik de gefilterde lijst te zien
+      },
+    });
+  }
+}
